781cfaf843a32be633add603be9a82a3ab067ef6
roland <roland@catalogix.se>
2024-09-07 08:48:58 +0200

    Updating to reflect changes in idpyoidc and fedservice

--- tests/entities/wallet.py
+++ tests/entities/wallet.py
@@ -36,6 +36,16 @@ PID_EEA_CONSUMER_CONFIG = {
             "kwargs": {
                 'dpop_signing_alg_values_supported': ["ES256"]
             }
+        },
+        "par": {
+            "function": "idpyoidc.client.oauth2.add_on.par.add_support",
+            "kwargs": {
+                "authn_method": {
+                    "client_assertion": {
+                        "class": "openid4v.client.client_authn.ClientAssertion"
+                    }
+                }
+            }
         }
         # "par": {
         #     "function": "idpyoidc.client.oauth2.add_on.par.add_support",
